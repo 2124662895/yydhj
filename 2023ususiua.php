39e5065cf1ccbb6b52a74c8274c06694f3fd09ba
2124662895 <160257000+2124662895@users.noreply.github.com>
2025-05-08 11:21:14 +0800

    Add files via upload

--- 2023ususiua.php
+++ 2023ususiua.php
@@ -1,64 +1,41 @@
 {
   "urls": [
   {
-      "url": "http://年年幸福.icu:88/2823/api.json",
-      "name": "幸福年年❤️❤️仓库-主用接口"
+      "url": "https://ys.mohuajz.eu.org/2823/api.json",
+      "name": "幸福年年❤️仓库-主用接口"
+    },
+  {
+      "url": "https://ys.mohuajz.eu.org/柒豪/柒豪4K.json",
+      "name": "幸福年年❤️仓库-柒豪"
+    },  
+    {
+      "url": "https://9877.kstore.space/AnotherD/api.json",
+      "name": "幸福年年❤️仓库-潇洒影视"
     },
     {
-      "url": "http://年年幸福.icu:88/短剧/22.txt",
-      "name": "幸福年年❤️❤️仓库-蓝莓短剧"
+      "url": "https://7337.kstore.space/twvip/自用测试.json",
+      "name": "幸福年年❤️仓库-天微"
     },
   {
       "url": "https://11293.kstore.space/tangwudong.json",
       "name": "💕幸福年年❤️-唐舞桐影视💓"
     },  
-    {
-      "url": "https://12523.kstore.space/hx.json",
-      "name": "💕幸福年年❤️-小桃4K🍋"
     },
     {
-      "url": "https://12523.kstore.space/hx.json",
-      "name": "💕幸福年年❤️-4K接口💓"
-    },
-    {
-      "url": "http://年年幸福.icu:88/摸鱼儿/moyu.json",
+      "url": "http://我不是.摸鱼儿.com",
       "name": "💕幸福年年❤️-摸鱼儿💓"
     },
     {
-      "url": "http://年年幸福.icu:88/王二小/api.json",
+      "url": "https://9280.kstore.space/wex.json",
       "name": "💕幸福年年❤️-王二小💓"
     },
    {
-      "url": "http://年年幸福.icu:88/小米/api.json",
+      "url": "http://miqk.cc/%E5%B0%8F%E7%B1%B3/DEMO.json",
       "name": "💕幸福年年❤️-小米💓"
-    },
-    {
-      "url": "http://年年幸福.icu:88/xy/kgz.json",
-      "name": "幸福年年❤️❤️仓库-小羊"
-    },
-    {
-      "url": "http://年年幸福.icu:88/天天开心/2024.php",
-      "name": "幸福年年❤️❤️仓库-天天开心"
-    },
-    {
-      "url": "http://年年幸福.icu:88/小橙子/xczwzx.json",
-      "name": "幸福年年❤️❤️仓库-小橙子"
     },
     {
       "url": "http://tv.laohu.cool/tvbox.json",
       "name": "幸福年年❤️❤️仓库-秒播"
-    },
-    {
-      "url": "http://年年幸福.icu:88/江楠楠/江楠楠.json",
-      "name": "💕幸福年年❤️-江楠楠影视💓"
-    },
-    {
-      "url": "http://年年幸福.icu:88/戏曲音乐/api.json",
-      "name": "💕幸福年年❤️-戏曲音乐💓"
-    },
-    {
-      "url": "http://年年幸福.icu:88/潇洒/api.json",
-      "name": "💕幸福年年❤️-潇洒💓"
     },
     {
       "url": "http://110.40.74.165:666/yunfensi/yunfensi4k.json",
@@ -85,20 +62,12 @@
       "name": "💕幸福年年❤️-肥猫🐈💓"
     },
     {
-      "url": "https://11405.kstore.space/xiaye/4k.json",
-      "name": "💕幸福年年❤️-夏夜4K💕"
-    },
-    {
-      "name": "新年快乐-影视接口✨✨",
-      "url": "https://codeberg.org/mzjk0/api/raw/branch/main/vip.json"
-    },
-    {
       "url": "https://6492.kstore.space/xnf/xnf.json",
       "name": "幸福年年❤️❤️仓库🏚️>🐅环 宇 轩"
     },
     {
       "name": "🎁🥮幸福年年❤️❤️仓库/书虫影视",
-      "url": "https://d.kstore.dev/download/6726/%E4%B9%A6%E8%99%AB%E5%BD%B1%E8%A7%86%E4%B8%93%E7%89%88%EF%BC%88%E5%A4%87%E7%94%A8%E7%BA%BF%E8%B7%AF%EF%BC%89.txt"
+      "url": "https://d.kstore.dev/download/6726/%E4%B9%A6%E8%99%AB%E5%BD%B1%E8%A7%86%E4%B8%93%E7%89%99%EF%BC%99%E5%A4%87%E7%94%A8%E7%BA%BF%E8%B7%AF%EF%BC%89.txt"
     },
   {
       "name": "🎁🥮幸福年年❤️❤️仓库/瓜子影视",
@@ -107,50 +76,6 @@
     {
       "name": "🎁🥮幸福年年❤️❤️仓库/白云智教",
       "url": "https://gitee.com/ltby1/blue-sky-education/raw/master/蓝天白云智教.json"
-    },
-    {
-      "name": "幸福年年❤️❤️-🌺兄弟影视",
-      "url": "http://ficfac.com/box/box.json"
-    },
-    {
-      "url": "http://mdxgh.tpddns.cn:9999/md.json",
-      "name": "幸福年年❤️❤️-码点玫瑰仓 🌹"
-    },
-    {
-      "name": "🎁🥮幸福年年❤️❤️仓库/影院",
-      "url": "https://gitlab.hk/iii/qq/-/raw/main/20.json"
-    },
-    {
-      "name": "🎁🥮幸福年年❤️❤️仓库/爱梦影视❤️",
-      "url": "https://d.kstore.dev/download/6375/爱梦单仓免费接口禁止贩卖.json"
-    },
-    {
-      "url": "https://6446.kstore.vip/fmys.json",
-      "name": "幸福年年❤️❤️仓库🌺🏠驸马影视"
-    },
-    {
-      "name": "❤️🥮幸福年年❤️❤️仓库/教育",
-      "url": "https://d.kstore.dev/download/4684/api.json?password=53889b4301c033c63bbb0cd0e6d07001-1732730182556"
-    },
-    {
-      "name": "❤️🥮幸福年年❤️❤️仓库/影视",
-      "url": "https://d.kstore.dev/download/4684/1api.json?password=d0e28b9dbb9d51773f231238b787789c-1732729526488"
-    },
-    {
-      "name": "❤️🥮幸福年年❤️❤️仓库/刺刺音乐",
-      "url": "https://d.kstore.dev/download/8975/刺刺专用音乐接口.json"
-    },
-    {
-      "name": "❤️🥮幸福年年❤️❤️仓库/王二小",
-      "url": "http://tvbox.王二小放牛娃.top"
-    },
-    {
-      "name": "❤️🥮幸福年年❤️❤️仓库/DG音乐",
-      "url": "https://ddzb.fun/dgmusic/api.json"
-    },
-    {
-      "url": "https://tv.nxog.top/m/111.php?ou=公众号欧歌app&mz=index&jar=index&123&b=tv",
-      "name": "幸福年年❤️影视💓|欧歌线路（影视专用）"
     },
     {
       "name": "小米/💚🥮幸福年年❤️❤️仓库",
